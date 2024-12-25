8e918f14485eddfec786dcd65fc7cdb842c3f8eb
German <german.chernysh1@gmail.com>
2024-12-25 22:05:58 +0400

    flake8 checked

--- python_01/ex02/load_image.py
+++ python_01/ex02/load_image.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PIL import Image
 
+
 def ft_load(path: str) -> np.ndarray:
     """
     Loads an image, prints its format, shape, and pixel content in RGB format.
@@ -14,22 +15,16 @@ def ft_load(path: str) -> np.ndarray:
     """
     try:
         img = Image.open(path)
-        
         if img.format not in ["JPEG", "JPG"]:
-            raise ValueError("Unsupported image format. Only JPG and JPEG are allowed.")
-        
+            raise ValueError("Only JPG and JPEG are allowed.")
         print(f"The format of the image is: {img.format}")
-        
         img_array = np.array(img)
-        
         print(f"The shape of the image is: {img_array.shape}")
-        
         return img_array
-    
+
     except FileNotFoundError:
         print("Error: The file was not found. Please check the file path.")
     except ValueError as ve:
         print(f"Error: {ve}")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
-
